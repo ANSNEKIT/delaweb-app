f506d945f2244d8b76a38955a9a495d959957a41
ansnekit <ansnekit@gmail.com>
2021-06-15 20:00:32 +0300

    deploy1

---
+++ precache-manifest.214a606b7aec4d65df3bf695ce516d01.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef8271919120d73438b",
+    "url": "/delaweb-app/css/app.eb9534f8.css"
+  },
+  {
+    "revision": "4154bd213fef8f9b9b6c",
+    "url": "/delaweb-app/css/chunk-0a837c04.066cf821.css"
+  },
+  {
+    "revision": "e6f8ccf213b60767046a",
+    "url": "/delaweb-app/css/chunk-vendors.2e22dcc9.css"
+  },
+  {
+    "revision": "3a7c887b36feaa8aa3fe81420f68d197",
+    "url": "/delaweb-app/index.html"
+  },
+  {
+    "revision": "0ef8271919120d73438b",
+    "url": "/delaweb-app/js/app.e9188718.js"
+  },
+  {
+    "revision": "4154bd213fef8f9b9b6c",
+    "url": "/delaweb-app/js/chunk-0a837c04.5b861dad.js"
+  },
+  {
+    "revision": "7281fb8f8aceda5c5503",
+    "url": "/delaweb-app/js/chunk-2fe668cb.f984cb87.js"
+  },
+  {
+    "revision": "e6f8ccf213b60767046a",
+    "url": "/delaweb-app/js/chunk-vendors.b2fa881b.js"
+  },
+  {
+    "revision": "e0d688f927696fb4c9b1cb6675e26ffd",
+    "url": "/delaweb-app/manifest.json"
+  }
+]);
